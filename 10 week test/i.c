00246a6bdd2e8bfde174eee3ba062d2e067e6934
tianjinsa <tianjinsa@outlook.com>
2024-11-10 13:38:29 +0800

    去掉缩小范围步骤

--- 10 week test/i.c
+++ 10 week test/i.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<math.h>
-double xy[301][2],tt,xxxx,yyyy,xxx,yyy;
+double xy[301][2],xxxx,yyyy;
 int nn;
 double ss(double a,double b,double x,double y)
 {
@@ -11,12 +11,12 @@ void yuanxjs(double x1,double y1,double x2,double y2,int fl)
     if(fl==0){
         fl=-1;
     }
-    xxx=(x1+x2)/2;
-    yyy=(y1+y2)/2;
+    xxxx=(x1+x2)/2;
+    yyyy=(y1+y2)/2;
     double detx=sqrt(1-pow(ss(x1,y1,x2,y2)/2,2)),d=atan((x1-x2)/(y1-y2)*-1);
     double dy=sin(d),dx=cos(d);
-    xxxx=detx*dx*fl+xxx;
-    yyyy=detx*dy*fl+yyy;
+    xxxx+=detx*dx*fl;
+    yyyy+=detx*dy*fl;
 }
 int main()
 {
@@ -30,7 +30,7 @@ int main()
         int xx=0,maxn=1;
         for(int i=0;i<nn;i++){
             for(int u=i+1;u<nn;u++){
-                if(ss(xy[i][0],xy[i][1],xy[u][0],xy[u][1])<=2.0002){
+                if(ss(xy[i][0],xy[i][1],xy[u][0],xy[u][1])<=2){
                     for(int g=0;g<=1;g++){
                         yuanxjs(xy[i][0],xy[i][1],xy[u][0],xy[u][1],g);
                         xx=0;
