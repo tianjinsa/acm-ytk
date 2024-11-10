26bf0257c0fa4630b4dc25887babd9b3d034a47d
tianjinsa <tianjinsa@outlook.com>
2024-11-10 13:24:09 +0800

    1

--- 10 week test/i.c
+++ 10 week test/i.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
-double xy[301][3],tt,max,xxxx,yyyy,xxx,yyy;
-int nn,ax;
+double xy[301][2],tt,xxxx,yyyy,xxx,yyy;
+int nn;
 double ss(double a,double b,double x,double y)
 {
     return sqrt(pow(a-x,2)+pow(b-y,2));
@@ -18,31 +18,18 @@ void yuanxjs(double x1,double y1,double x2,double y2,int fl)
     xxxx=detx*dx*fl+xxx;
     yyyy=detx*dy*fl+yyy;
 }
-
-
-
-int aim(double x,double y,int n)
+int main()
 {
-    max=0;
-    for(int i=0;i<nn;i++){
-        if(xy[i][2]>=0){
-            tt=ss(x,y,xy[i][0],xy[i][1]);
-            if(tt>max){
-                max=tt;
-                ax=i;
-            }
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d",&nn);
+        for(int i=0;i<nn;i++){
+            scanf("%lf%lf",&xy[i][0],&xy[i][1]);
         }
-    }
-    if(max>1.0999){
-        x=(x*n-xy[ax][0])/(n-1);
-        y=(y*n-xy[ax][1])/(n-1);
-        xy[ax][2]=-n;
-        return aim(x,y,n-1);
-    }
-    int xx=0,maxn=1;
-    for(int i=0;i<nn;i++){
-        for(int u=i+1;u<nn;u++){
-            if(i!=u&&(xy[i][2]>=-n-2)&&(xy[u][2]>=-n-2)){
+        int xx=0,maxn=1;
+        for(int i=0;i<nn;i++){
+            for(int u=i+1;u<nn;u++){
                 if(ss(xy[i][0],xy[i][1],xy[u][0],xy[u][1])<=2.0002){
                     for(int g=0;g<=1;g++){
                         yuanxjs(xy[i][0],xy[i][1],xy[u][0],xy[u][1],g);
@@ -57,31 +44,9 @@ int aim(double x,double y,int n)
                         }
                     }
                 }
-                
             }
         }
-    }
-    return maxn;
-}
-int main()
-{
-    double xx,yy;
-    int t,n;
-    scanf("%d",&t);
-    while(t--){
-        scanf("%d",&n);
-        nn=n;
-        xx=0;yy=0;
-        for(int i=0;i<n;i++){
-            scanf("%lf%lf",&xy[i][0],&xy[i][1]);
-            xx+=xy[i][0];
-            yy+=xy[i][1];
-            xy[i][2]=1;
-        }
-        xx/=n;
-        yy/=n;
-        n=aim(xx,yy,n);
-        printf("%d\n",n);
+        printf("%d\n",maxn);
     }
     return 0;
 }
