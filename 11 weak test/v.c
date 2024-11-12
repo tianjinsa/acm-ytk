cb2ac6d011d3388ee69829905f1a63cd7816a163
tianjinsa <tianjinsa@outlook.com>
2024-11-12 18:22:22 +0800

    删除不必要的文件，优化 v.c 中的根计算逻辑

--- 11 weak test/v.c
+++ 11 weak test/v.c
@@ -8,27 +8,20 @@ int main()
         double a,b,c,x1,x2,tt;
         scanf("%lf%lf%lf",&a,&b,&c);
         tt=b*b-4*a*c;
-        if(tt>0){
-            x1=(-b+sqrt(tt))/(2*a);
-            x2=(-b-sqrt(tt))/(2*a);
-            if(x1<x2){
-                double t=x1;
-                x1=x2;
-                x2=t;
-            }
-            long long  xxx=100000*x1,yyy=100000*x2;
-            if(xxx==yyy){
-                goto to;
-            }
-            printf("x1=%.5lf;x2=%.5lf\n",x1,x2);
-        }
-        else if(b*b==4*a*c){
+        if(fabs(tt)<1e-8){
             x1=(-b)/(2*a);
-            to:
             printf("x1=x2=%.5lf\n",x1);
         }
+        else if(tt>0){
+            x1=(-b+sqrt(tt))/(2*a);
+            x2=(-b-sqrt(tt))/(2*a);
+            printf("x1=%.5lf;x2=%.5lf\n",x1,x2);
+        }
         else{
-            x1=(-b)/(2*a);
+            x1=-b/(2*a);
+            if(fabs(x1)<1e-8){
+                x1=0;
+            }
             x2=sqrt(-tt)/(2*a);
             printf("x1=%.5f+%.5fi;x2=%.5f-%.5fi\n",x1,x2,x1,x2);
         }
