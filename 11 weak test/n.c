80a6175db4266221c07077a4d254aef8d3b65ee4
JiaSurui <107697707+tianjinsa@users.noreply.github.com>
2024-11-14 15:52:09 +0800

    更新 11 weak test

---
+++ 11 weak test/n.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n--){
+        int a[100]={0},x,y;
+        scanf("%d%d",&x,&y);
+        a[x]=1;
+        for(int i=x+1;i<=y;i++){
+            a[i]=a[i-1]+a[i-2];
+        }
+        printf("%d\n",a[y]);
+    }
+    
+    return 0;
+}
