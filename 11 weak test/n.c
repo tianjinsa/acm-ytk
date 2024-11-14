09eb42c07bf174d52844012b59e8e24430357c15
JiaSurui <107697707+tianjinsa@users.noreply.github.com>
2024-11-14 16:09:49 +0800

    更新 n.c

--- 11 weak test/n.c
+++ 11 weak test/n.c
@@ -7,6 +7,7 @@ int main()
     while(n--){
         int a[100]={0},x,y;
         scanf("%d%d",&x,&y);
+        a[x-1]=0;
         a[x]=1;
         for(int i=x+1;i<=y;i++){
             a[i]=a[i-1]+a[i-2];
