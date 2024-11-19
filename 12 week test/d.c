a2260654b51811bbbf38649528ba0b18f8aadb9b
tianjinsa <tianjinsa@outlook.com>
2024-11-19 10:57:36 +0800

    1

--- 12 week test/d.c
+++ 12 week test/d.c
@@ -5,25 +5,30 @@ int main()
     int n;
     scanf("%d",&n);
     while(n--){
-        char abc[15]={0};
+        int abc[20][2]={0};
         char a[2][6],c[6];
         for(int i=0; i<3; i++) {
             scanf(" %s%s%s",a[0],a[1],c);
             if(strcmp(c,"even")==0){
                 for(int j=0;j<2;j++){
                     for(int h=0;h<4;h++){
-                        abc[a[j][h]-'A']=2;
+                        abc[a[j][h]-'A'][0]=2;
+                        abc[a[j][h]-'A'][1]=0;
                     }
                 }
             }
             else if(strcmp(c,"up")==0){
                 for(int j=1;j<=3;j+=2){
                     for(int h=0; h<4; h++){
-                        if(abc[a[j][h]-'A']==0){
-                            abc[a[j][h]-'A']=j;
+                        if(abc[a[j][h]-'A'][0]==0){
+                            abc[a[j][h]-'A'][0]=j;
                         }
-                        else if(abc[a[j][h]-'A']!=j){
-                            abc[a[j][h]-'A']=2;
+                        else if(abc[a[j][h]-'A'][0]!=j){
+                            abc[a[j][h]-'A'][0]=2;
+                            abc[a[j][h]-'A'][1]=0;
+                        }
+                        else if(abc[a[j][h]-'A'][0]==j){
+                            abc[a[j][h]-'A'][1]=1;
                         }
                     }
                 }
@@ -31,20 +36,30 @@ int main()
             else {
                 for(int j=3;j>=1;j-=2){
                     for(int h=0;h<4;h++){
-                        if(abc[a[j][h]-'A']==0){
-                            abc[a[j][h]-'A']=j;
+                        if(abc[a[j][h]-'A'][0]==0){
+                            abc[a[j][h]-'A'][0]=j;
                         }
-                        else if(abc[a[j][h]-'A']!=j){
-                            abc[a[j][h]-'A']=2;
+                        else if(abc[a[j][h]-'A'][0]!=j){
+                            abc[a[j][h]-'A'][0]=2;
+                            abc[a[j][h]-'A'][1]=0;
+                        }
+                        else if(abc[a[j][h]-'A'][0]==j){
+                            abc[a[j][h]-'A'][1]=1;
                         }
                     }
                 }
             }
         }
         for(char j='A';j<='L';j++){
-            if(abc[j-'A']==1||abc[j-'A']==3){
-                printf("%c is the counterfeit coin and it is %s.\n",j,abc[j-'A']==1?"light":"heavy");
-                break;
+            if(abc[j-'A'][1]==1){
+                printf("%c is the counterfeit coin and it is %s.\n",j,abc[j-'A'][0]==1?"light":"heavy");
+                continue;
+            }
+        }
+        for(char j='A';j<='L';j++){
+            if(abc[j-'A'][0]==1||abc[j-'A'][0]==3){
+                printf("%c is the counterfeit coin and it is %s.\n",j,abc[j-'A'][0]==1?"light":"heavy");
+                continue;
             }
         }
 
