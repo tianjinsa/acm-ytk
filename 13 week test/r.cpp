78a55a2bfe77f5281e1d27df1392498398993faa
tianjinsa <tianjinsa@outlook.com>
2024-11-27 22:27:45 +0800

    修复字符串处理逻辑，确保只返回有效匹配，优化内存使用

--- 13 week test/r.cpp
+++ 13 week test/r.cpp
@@ -1,40 +1,44 @@
 #include<bits/stdc++.h>
 //using namespace std;
-char abc[21*2001],ch[2001][21],x[21];
+char abc[21*2001+1],ch[2001][21],x[21],temp[21*2001+1];
 int sss(char x[])
 {
     int n=0;
     char *p;
-    char temp[21*2001];
     strcpy(temp,abc);
     while(p=strstr(temp,x)){
         n++;
         if((p-temp)%21){
             n--;
         }
+        if(n>1){
+            return 0;
+        }
         memset(temp+(p-temp)/21*21,1,21);
     }
-    return n;
+    return 1;
 }
 int main()
 {
     int n=0;
     char a;
     memset(abc,1,sizeof(abc));
-    while(scanf("%s",ch[n])!=EOF){
+    abc[21*2001]='\0';
+    while(scanf("%s",ch[n])!=EOF&&ch[n][0]!='#'){
         for(int i=0;ch[n][i]!='\0';i++){
             abc[n*21+i]=ch[n][i];
         }
         n++;
     }
-    bool fl=1;
+    bool fl;
     for(int i=0;i<n;i++){
+        fl=1;
         for(int j=1;j<strlen(ch[i]);j++){
             for(int k=0;k<j;k++){
                 x[k]=ch[i][k];
             }
             x[j]='\0';
-            if(sss(x)==1){
+            if(sss(x)){
                 printf("%s %s\n",ch[i],x);
                 fl=0;
                 break;
