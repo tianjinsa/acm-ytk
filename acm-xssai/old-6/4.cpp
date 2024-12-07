a5224d99ba6727949c6240a307e10942add9f137
tianjinsa <tianjinsa@outlook.com>
2024-12-07 11:05:57 +0800

    Changes

--- acm-xssai/old-6/4.cpp
+++ acm-xssai/old-6/4.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 //using namespace std;
-int abc[500][500];
+char abc[500][500];
 void clean(int a,int b,int x,int y)
 {
     for(int i=a;i<=x;i++){
         for(int j=b;j<=y;j++){
-            abc[i][j]=0;
+            abc[i][j]='0';
         }
     }
 }
@@ -13,7 +13,7 @@ int chack(int a,int b,int x,int y)
 {
     for(int i=a;i<a+x;i++){
         for(int j=b;j<b+y;j++){
-            if(abc[i][j]==0){
+            if(abc[i][j]=='0'){
                 clean(a,b,i,j);
                 return 0;
             }
@@ -26,19 +26,20 @@ int main()
     int n;
     scanf("%d",&n);
     while(n--){
-        int a,b,x,y;
+        int a,b,x,y,tt;
         scanf("%d%d",&a,&b);
         
         for(int i=0;i<a;i++){
             for(int j=0;j<b;j++){
-                scanf("%d",&abc[i][j]);
+                scanf("%d",&tt);
+                abc[i][j]=tt+'0';
             }
         }
         bool fl=0;
         scanf("%d%d",&x,&y);
         for(int i=0;i<=a-x;i++){
             for(int j=0;j<=b-y;j++){
-                if(chack(i,j,x,y)){
+                if(abc[i][j]=='1'&&chack(i,j,x,y)){
                     fl=1;
                     goto to;
                 }
