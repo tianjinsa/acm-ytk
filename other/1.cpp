ed07b98e6f8dcd4cfc218d87f74644fdd1f5c10e
tianjinsa <tianjinsa@outlook.com>
2024-12-09 16:01:53 +0800

    1

---
+++ other/1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+map<string, int> mp;
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n--){
+        string s;
+        int x,y;
+        scanf("%d%d",&x,&y);
+        if(y<0){
+            y=-y;
+        }
+        s=to_string(x)+" "+to_string(y);
+        if(mp.find(s)==mp.end()){
+            mp[s]=1;
+        }
+    }
+    printf("%lld\n",mp.size());
+    return 0;
+}
