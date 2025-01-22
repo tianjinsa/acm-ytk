bac24f6063b5d3b868d52409927fa6e56cdad9e1
tianjinsa <tianjinsa@outlook.com>
2025-01-23 00:19:53 +0800

    1

--- 寒假训练/2/h.cpp
+++ 寒假训练/2/h.cpp
@@ -1,8 +1,26 @@
 #include<bits/stdc++.h>
-//using namespace std;
-
+using namespace std;
+int sss(const string a,const string b) {
+    int n=a.size(),m=b.size();
+    int dp[2][10001]={0};
+    for(int i=1;i<=n;i++){
+        int tt=i%2;
+        dp[tt][0]=0;
+        for(int j=1;j<=m;j++){
+            if(a[i-1]==b[j-1]){
+                dp[tt][j]=dp[!tt][j-1]+1;
+            }
+            else{
+                dp[tt][j]=max(dp[!tt][j],dp[tt][j-1]);
+            }
+        }
+    }
+    return dp[n%2][m];
+}
 int main()
 {
-    
+    string a,b;
+    cin>>a>>b;
+    cout<<sss(a,b);
     return 0;
 }
