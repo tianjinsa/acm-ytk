84b67cce876ae61db0d31bf193c66eee0ec9f3ec
tianjinsa <tianjinsa@outlook.com>
2025-04-17 20:07:51 +0800

    1

---
+++ 做题合集/pta/模拟赛/14.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>>a;
+vector<int>tree;
+
+void update(int index,int vv){
+    while(index<tree.size()){
+        tree[index]+=vv;
+        index+=index&(-index);
+    }
+}
+int query(int index){
+    int ans=0;
+    while(index>0){
+        ans+=tree[index];
+        index-=index&(-index);
+    }
+    return ans;
+}
+int query(int l,int r){
+    return query(r)-query(l-1);
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    a.resize(n,vector<int>(n,0));
+    for(int i=0;i<n;i++)
+        cin>>v[i];
+    for(int i=0;i<n;i++){
+        tree.assign(n+1,0);
+        update(v[i],1);
+        for(int j=i+1;j<n;j++)
+        {
+            int t=query(v[j],tree.size()-1);
+            a[i][j]=a[i][j-1]+t;
+            update(v[j],1);
+        }
+    }
+
+    bool fl=0;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            int tt=(j-i)*(j-i+1)/2;
+            if(fl)cout<<' ';
+            fl=1;
+            cout<<a[0][n-1]-2*a[i][j]+tt;
+        }
+    }
+    return 0;
+}
