775daf4c3402baecc2ec890fd463f7d7e8a60e8f
tianjinsa <tianjinsa@outlook.com>
2025-09-19 13:38:23 +0800

    更新 VSCode 配置文件，优化 C++ 构建和调试设置，添加文件关联，重构代码以提高可读性

--- 做题合集/vjudge/【ACM2025-作业1】简单题/i.cpp
+++ 做题合集/vjudge/【ACM2025-作业1】简单题/i.cpp
@@ -1,123 +1,124 @@
 #include<bits/stdc++.h>
 using namespace std;
-long double PI=acos(-1);
-using ld=long double;
-ld r,c;
-struct node{
-    bool fl;
-    ld x1,y1,x2,y2,ox,oy;
-    bool arc0to90;
-};
-struct Point { ld x,y; };
-static inline bool eq(ld a, ld b) {
-    return fabsl(a - b) < 1e-9L;
-}
-ld dist(Point a,Point b){
-    return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
-}
-// 计算圆弧与线段的交点并返回交点及连线与x轴的弧度
-tuple<ld,ld,ld> intersectArcSegment(ld cx,ld cy,ld x1,ld y1,ld x2,ld y2,bool arc0to90)
-{
-    Point pt;
-    if (eq(y1, y2)) {
-        ld y=y1;
-        ld d=r*r-(y-cy)*(y-cy);
-        pt={cx + (arc0to90 ? sqrt(d) : -sqrt(d)),y};
-    } else {
-        ld x=x1;
-        ld d=r*r-(x-cx)*(x-cx);
-        pt={x,cy + sqrt(d)};
-    }
-    ld ang=atan2(pt.y-cy,pt.x-cx);
-    return {pt.x,pt.y,ang};
-}
+using lf=long double;
+lf PI=acos(-1);
+lf r,c;
 
-// 计算两个固定圆弧(第一弧90~0,第二弧180~90)的交点并返回交点及两弧对应连线与x轴弧度
-tuple<ld,ld,ld,ld> intersectArcArc(ld cx1,ld cy1,ld cx2,ld cy2)
-{
-    ld dx=cx2-cx1,dy=cy2-cy1;
-    ld d=sqrt(dx*dx+dy*dy);
-    ld mx=(cx1+cx2)/2,my=(cy1+cy2)/2;
-    ld h=sqrt(r*r-(d/2)*(d/2));
-    ld rx=-dy*(h/d),ry=dx*(h/d);
-    Point p1={mx+rx,my+ry};
-    Point p2={mx-rx,my-ry};
-    for (auto p : {p1,p2}) {
-        bool ok1 = (p.x >= cx1 && p.y >= cy1);  // 第一弧90~0
-        bool ok2 = (p.x <= cx2 && p.y >= cy2);  // 第二弧180~90
-        if (ok1 && ok2) {
-            ld ang1=atan2(p.y-cy1,p.x-cx1);
-            ld ang2=atan2(p.y-cy2,p.x-cx2);
-            return make_tuple(p.x,p.y,ang1,ang2);
-        }
-    }
-    // 默认返回第一个点
-    ld ang1=atan2(p1.y-cy1,p1.x-cx1);
-    ld ang2=atan2(p1.y-cy2,p1.x-cx2);
-    return make_tuple(p1.x,p1.y,ang1,ang2);
+lf arcx(lf dy){//弦长求最小圆心距离
+    return r*sin(PI/2-asin((r-dy)/r));
 }
+lf arcl(lf dx,lf dy){//弦长求弧长
+    return r*(2*asin(sqrt(dx*dx+dy*dy)/(2*r)));
+}
+static const lf EPS = 1e-9;
+inline bool eq(lf a, lf b) { return fabsl(a - b) < EPS; }// a == b
+inline bool le(lf a, lf b) { return a - b <= EPS; }// a <= b
+inline bool ge(lf a, lf b) { return a - b >= -EPS; }// a >= b
+inline bool lt(lf a, lf b) { return a - b < -EPS; }// a < b
+inline bool gt(lf a, lf b) { return a - b > EPS; }// a > b
 int main()
 {
-
     int n;
+    int nn=1;
     while(cin>>r>>n&&(r||n)){
-
         c=2*r*PI;
-        ld ans=0;
-        ld x,y,mx=0,my=0;
-        vector<node>aa;
+        lf x,y,mx=0,my=0,ans=0;
+        vector<tuple<bool,lf,lf,lf>> v;
         for(int i=0;i<n;i++){
+            
             cin>>x>>y;
-            if(y>0){
-                if(x<=r*2){
-                    auto[fl,x1,y1,x2,y2,ox,oy,arc0to90]=aa.back();
-                    
-                    if(!fl&&eq(x1,x2)&&y2<y1){
-                        aa.pop_back();
-                        if(y1>=my+y){
-                            y2=my+y;
-                            mx+=x;
-                            my+=y;
-                            if(dist({x1,y1},{mx+x,my+y})>r){
-                                aa.push_back({fl,x1,y1,x2,y2,ox,oy,arc0to90});
+            v.push_back({0,my,mx,mx+x});
+            mx+=x;
+            v.push_back({1,mx,my,my+y});
+            my+=y;
+        }
+        auto&[fl,xy,d1,d2]=v[0];
+        mx=(d2-d1)-r,my=r;
+        ans+=(d2-d1)-r;
+        n=v.size();
+        for(int i=1;i<n;i++){
+            auto &[fl,xy,d1,d2]=v[i];
+            if(fl){//垂直
+                if(d2<d1){//向下
+                    lf h = d1 - d2;
+                    lf bestAngle = PI;
+                    int bestJ = i;
+                    int m2 = v.size();
+                    for(int j = i+1; j < m2; j++){
+                        auto& [fl2, xy2, d12, d22] = v[j];
+                        lf dx = fabsl(xy2 - xy);
+                        if(dx > 2*r) break;
+                        if(fl2){
+                            lf dh = d22 - d12;
+                            if(dh > 0 && dh < h){
+                                lf ang = acos((r-h)/r) + acos((r-dh)/r);
+                                if(ang < bestAngle){ bestAngle = ang; bestJ = j; }
                             }
-                            else{
-                                auto& ce=aa.back();
-                                auto [px,py,ang1,ang2]=intersectArcArc(ce.ox,ce.oy,mx,my);
-                                if(py<my-y+r){
-                                    aa.push_back({0,mx-x,my-y+r,mx,my-y+r});
-                                }
+                        } else {
+                            lf dh = xy2 - d2;
+                            if(dh >= -EPS) {
+                                lf ang = acos((r-h)/r);
+                                lf xHit = xy + r * sin(ang);
+                                if(xHit + EPS >= min(d12, d22) && xHit <= max(d12, d22) + EPS){ bestAngle = ang; bestJ = j; }
                             }
-                            aa.push_back({1,mx-r,my,mx,my+r,mx,my,0});
-                        }
-                        else{
-
                         }
                     }
+                    // 如果旋转90度内无碰撞，则垂直下降至水平边
+                    if(bestAngle >= PI/2) {
+                        lf drop = 1e18;6
+                        int hitJ = -1;
+                        for(int j = i+1; j < m2; j++){
+                            auto& [fl2, xy2, d12, d22] = v[j];
+                            if(!fl2){
+                                if(mx + EPS >= d12 && mx <= d22 + EPS){
+                                    lf dropDist = my - (xy2 + r);
+                                    if(dropDist > EPS && dropDist < drop){ drop = dropDist; hitJ = j; }
+                                }
+                            }
+                        }
+                        if(hitJ != -1){
+                            ans += drop;
+                            my -= drop;
+                            i = hitJ;
+                            continue;
+                        }
+                    }
+                    ans += r * bestAngle;
+                    // 若落在上升沿，旋转至最近水平边
+                    auto& [flB, xyB, d1B, d2B] = v[bestJ];
+                    if(flB && d2B > d1B) {
+                        lf dh2 = d2B - d1B;
+                        lf ang2 = acos((r - dh2)/r);
+                        ans += r * ang2+r;
+                        mx = xyB + r;
+                        my = d2B + r;
+                    }
+                    else{
+                        mx = d1B+r;
+                        ans+=r-arcx(d1-xyB);
+                        my = xyB+r;
+                    }
+                    i = bestJ;
                 }
-                else{
-                    aa.push_back({0,mx,my+r,mx+x,my+r});
-                    mx+=x;
-                    aa.push_back({0,mx-r,my,mx-r,my+y});
-                    my+=y;
-                    aa.push_back({1,mx-r,my,mx,my+r,mx,my,0});
+                else{//向上
+                    lf need=arcx(d2-d1);
+                    mx+=r+r;
+                    my+=d2-d1;
+                    ans+=r-need+r;
+                    if(r>d2-d1){//台阶小于半径
+                        ans+=arcl(need,d2-d1);
+                    }
+                    else{//台阶大于等于半径
+                        ans+=PI/2*r+(d2-d1-r);
+                    }
                 }
-                
-            }
-            else if(y<0){
-                aa.push_back({0,mx,my+r,mx+x,my+r});
-                mx+=x;
-                aa.push_back({1,mx,my+r,mx+r,my,mx,my,1});
-                if(-y>r){
-                    aa.push_back({0,mx+r,my,mx+r,my+y+r});
-                }
-                my+=y;
             }
             else{
-                aa.push_back({0,mx,my+r,mx+x,my+r});
-                mx+=x;
+                ans+=d2-d1-r*2;
+                mx+=d2-d1-r*2;
             }
         }
+        cout<<fixed<<setprecision(3)<<"Case "<<nn++<<": Distance = "<<ans<<endl;
     }
     return 0;
 }
--- 做题合集/vjudge/【ACM2025-作业1】简单题/x.cpp
+++ 做题合集/vjudge/【ACM2025-作业1】简单题/x.cpp
@@ -1,45 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+auto ss(string &s) {
+    long long pos=s.find('.');
+    if (pos == string::npos) {
+        s += ".0";
+        pos=s.size()-2;
+    }
+};
 int main() {
     string a,b;
     if (!(cin >> a >> b)) return 0;
-    auto normalize=[&](string &s) {
-        size_t pos=s.find('.');
-        if (pos == string::npos) {
-            s += ".0";
-            pos=s.size()-2;
-        }
-    };
-    normalize(a);
-    normalize(b);
+    
+    ss(a);
+    ss(b);
     int p1=a.find('.'),p2=b.find('.');
-    string int1=a.substr(0,p1),frac1=a.substr(p1+1);
-    string int2=b.substr(0,p2),frac2=b.substr(p2+1);
-    int L=max(frac1.size(),frac2.size());
-    while (frac1.size() < L) frac1.push_back('0');
-    while (frac2.size() < L) frac2.push_back('0');
-    string resFrac(L,'0');
-    int carry=0;
+    string int1=a.substr(0,p1),af=a.substr(p1+1);
+    string int2=b.substr(0,p2),bf=b.substr(p2+1);
+    int L=max(af.size(),bf.size());
+    while (af.size() < L) af.push_back('0');
+    while (bf.size() < L) bf.push_back('0');
+    string cf(L,'0');
+    int tem=0;
     for (int i=L-1; i >= 0; --i) {
-        int d=(frac1[i]-'0')+(frac2[i]-'0')+carry;
-        resFrac[i]=char('0'+d % 10);
-        carry=d / 10;
+        int d=(af[i]-'0')+(bf[i]-'0')+tem;
+        cf[i]=char('0'+d % 10);
+        tem=d / 10;
     }
     int n1=int1.size(),n2=int2.size();
     int i=n1-1,j=n2-1;
-    string resInt;
-    while (i >= 0 || j >= 0 || carry) {
-        int d=carry;
+    string ci;
+    while (i >= 0||j >= 0||tem) {
+        int d=tem;
         if (i >= 0) d += int1[i--]-'0';
         if (j >= 0) d += int2[j--]-'0';
-        resInt.push_back(char('0'+(d % 10)));
-        carry=d / 10;
+        ci.push_back(char('0'+(d % 10)));
+        tem=d / 10;
     }
-    while (resInt.size() > 1 && resInt.back() == '0') resInt.pop_back();
-    reverse(resInt.begin(),resInt.end());
-    int k=resFrac.size()-1;
-    while (k > 0 && resFrac[k] == '0') --k;
-    resFrac.resize(k+1);
-    cout << resInt << '.' << resFrac;
+    while (ci.size() > 1 && ci.back() == '0') ci.pop_back();
+    reverse(ci.begin(),ci.end());
+    int k=cf.size()-1;
+    while (k > 0 && cf[k] == '0') --k;
+    cf.resize(k+1);
+    cout<<ci<<'.'<<cf;
     return 0;
 }
